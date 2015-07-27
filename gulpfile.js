6bfc5c45aaa46c28eb7c505feab4d4b51b7e3caf
Henrik <henrik_mathiesen@hotmail.com>
2015-07-28 01:24:27 +0200

    Added default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task('hello-world', function(){
@@ -13,8 +14,13 @@ gulp.task('js', function() {
 		  './js/src/jsfile01.js', 
 		  './js/src/jsfile02.js'
 		 ])
+	.pipe(sourcemaps.init())
 	.pipe(concat('app.js'))
 	.pipe(gulp.dest('./js/bld/'))
 	.pipe(uglify())
+	.pipe(sourcemaps.write())
     .pipe(gulp.dest('./js/bld/'));
 });
+
+
+gulp.task('default', ['js']);
