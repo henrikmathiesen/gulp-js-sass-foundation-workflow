931710c79ad67bc5588fbd2f8d50216ecd578987
Henrik <henrik_mathiesen@hotmail.com>
2015-07-28 00:11:44 +0200

    More gulp packages, also git ignore js and sass builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var size = require('gulp-filesize');
+
 
 gulp.task('hello-world', function(){
 	console.log('Our very first Gulp task!');
+});
+
+gulp.task('js', function() {
+  return gulp.src(['./js/src/jsfile01.js', './js/src/jsfile02.js'])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./js/bld/'));
 });
