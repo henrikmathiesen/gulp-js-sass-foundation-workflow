545c267f8205d3fb28fcdd21fd3e697a9731b996
Henrik <henrik_mathiesen@hotmail.com>
2015-07-28 05:12:48 +0200

    gulp if added, for dev/prod flag

--- gulpfile.js
+++ gulpfile.js
@@ -5,43 +5,62 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
 
+var jsAppSrc = [
+	'./js/src/jsfile01.js',
+	'./js/src/jsfile02.js'
+];
+
+var jsLibSrc = [
+	'./bower_components/foundation/js/vendor/jquery.js',
+	'./bower_components/foundation/js/vendor/fastclick.js',
+	'./bower_components/foundation/js/foundation/foundation.js'
+];
+
+var sassSrc = './sass/src/app.scss';
+var sassSrcWatch = './sass/src/**/*.scss';
+var jsBld = './js/bld/';
+var sassBld = './sass/bld/';
+
+
 gulp.task('js-app', function () {
 	return gulp
-		.src([
-			'./js/src/jsfile01.js',
-			'./js/src/jsfile02.js'
-		])
+		.src(jsAppSrc)
 		.pipe(sourcemaps.init())
 		.pipe(concat('app.js'))
-		.pipe(gulp.dest('./js/bld/'))
+		.pipe(gulp.dest(jsBld))
 		.pipe(uglify())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./js/bld/'));
+		.pipe(gulp.dest(jsBld));
 });
 
 gulp.task('js-lib', function () {
 	return gulp
-		.src([
-			'./bower_components/foundation/js/vendor/jquery.js',
-			'./bower_components/foundation/js/vendor/fastclick.js',
-			'./bower_components/foundation/js/foundation/foundation.js'
-		])
+		.src(jsLibSrc)
 		.pipe(sourcemaps.init())
 		.pipe(concat('lib.js'))
-		.pipe(gulp.dest('./js/bld/'))
+		.pipe(gulp.dest(jsBld))
 		.pipe(uglify())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./js/bld/'));
+		.pipe(gulp.dest(jsBld));
 });
 
 gulp.task('sass', function () {
 	return gulp
-		.src('./sass/src/app.scss')
+		.src(sassSrc)
 		.pipe(sourcemaps.init())
-		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(sass({ outputStyle: 'compressed' }))
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./sass/bld/'));
+		.pipe(gulp.dest(sassBld));
 });
 
 
-gulp.task('default', ['js-app', 'js-lib', 'sass']);
+gulp.task('js-app-watcher', function () {
+	gulp.watch(jsAppSrc, ['js-app']);
+});
+
+gulp.task('sass-app-watcher', function(){
+	gulp.watch(sassSrcWatch, ['sass']);
+});
+
+
+gulp.task('default', ['js-app', 'js-lib', 'sass', 'js-app-watcher', 'sass-app-watcher']);
