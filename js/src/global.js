a36258760c7998d33dc3df86a6db5d2b5193e297
Henrik <henrik_mathiesen@hotmail.com>
2015-11-08 16:05:31 +0100

    - Comment

--- js/src/global.js
+++ js/src/global.js
@@ -2,7 +2,7 @@
 
 	$(document).foundation();
 	
-	// The one gloabl variable in the js application, put all globals on window.app
+	// The one global variable in the js application, put all globals on window.app
 	window.app = {};
 
 })();
