f4ba3329e2ca9d61ac85c86f4ff43f753d8eb96a
Henrik Mathiesen <henrik_mathiesen@hotmail.com>
2015-11-16 15:41:52 +0100

    -Preparing for ajax caching in two different ways

---
+++ js/src/ajax-cache-service.js
@@ -0,0 +1,19 @@
+window.app.ajaxCacheService = function(){
+	
+	var _cache = [];
+	
+	var pushToCache = function(){
+		
+	};
+	
+	var pullFromCache = function(){
+		
+	};
+	
+	
+	return {
+		pushToCache: pushToCache,
+		pullFromCache:pullFromCache
+	}
+	
+}();
--- js/src/render-ajax-content-directive.js
+++ js/src/render-ajax-content-directive.js
@@ -3,16 +3,47 @@
 	$('[data-render-ajax-content]').on('click', function (e) {
 		e.preventDefault();
 
-		var $renderContentSelector = $($(this).attr('data-render-container'));
-		var mustacheTemplate = window.app.eventMustacheTemplate();
-
-		// Ajax Content is rendered, no need to fetch and render on subsequent clicks
-		if ($renderContentSelector.find('>h2').length) { return; }
+		var $renderContentSelector = $($(this).attr('data-render-ajax-container'));
+		var mustacheTemplate =  eval($(this).attr('data-render-ajax-mustache-template'));
+		var cacheType = $(this).attr('data-render-ajax-cache-type');
 		
-		$.get('http://localhost:1338/getevents')
+		
+		var getAndRenderAjaxData = function(){
+			$.get('http://localhost:1338/getevents')
 				.then(function (data) {
 					$renderContentSelector.html(Mustache.render(mustacheTemplate, data[0]));
-				});
+				});	
+		};
+		
+		switch(cacheType) {
+			case "":
+				// No caching - always make ajax call when link is clicked
+				console.log("No caching");
+				getAndRenderAjaxData();
+				break;
+			case "js":
+				// Caching with javascript construct
+				// ...
+				console.log("JS caching");
+				break;
+			default:
+				// Caching with selector check - if selector is in DOM, then no need to make ajax call again
+				console.log("Selector caching");
+				if ($renderContentSelector.children($(cacheType)).length) { return; }
+				getAndRenderAjaxData();
+				break;
+		}
+
+
+		if((cacheType !== "js") || (cacheType !== "")) {
+			
+			// Ajax Content is rendered, no need to fetch and render on subsequent clicks
+			if ($renderContentSelector.children($(cacheType)).length) { return; }
+			
+		}
+
+		
+		
 
 	});
 
