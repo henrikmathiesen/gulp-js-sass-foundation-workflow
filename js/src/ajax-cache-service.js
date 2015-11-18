9b4410729ff6eb581d8a6cff47c2ef21da4a4a3f
Henrik Mathiesen <henrik_mathiesen@hotmail.com>
2015-11-18 13:09:41 +0100

    -implemented js object caching for ajax

--- js/src/ajax-cache-service.js
+++ js/src/ajax-cache-service.js
@@ -1,19 +1,35 @@
 window.app.ajaxCacheService = function(){
 	
-	var _cache = [];
-	
-	var pushToCache = function(){
-		
-	};
-	
-	var pullFromCache = function(){
-		
-	};
-	
-	
-	return {
-		pushToCache: pushToCache,
-		pullFromCache:pullFromCache
-	}
+	var _ajaxCache = [];
+
+    var checkIfItemIsInCache = function (item) {
+        for (var i = 0; i < _ajaxCache.length; i += 1) {
+            if (_ajaxCache[i].cachedItem === item) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    var pushToCache = function (item, response) {
+        _ajaxCache.push({ cachedItem: item, cachedResponse: response });
+        console.log(_ajaxCache);
+    };
+
+    var pullFromCache = function (item) {
+        for (var i = 0; i < _ajaxCache.length; i += 1) {
+            if (_ajaxCache[i].cachedItem === item) {
+                return _ajaxCache[i].cachedResponse;
+            }
+        }
+        return false;
+    };
+
+
+    return {
+        checkIfItemIsInCache: checkIfItemIsInCache,
+        pushToCache: pushToCache,
+        pullFromCache: pullFromCache
+    };
 	
 }();
--- js/src/render-ajax-content-directive.js
+++ js/src/render-ajax-content-directive.js
@@ -6,17 +6,29 @@
 		var $renderContentSelector = $($(this).attr('data-render-ajax-container'));
 		var mustacheTemplate =  eval($(this).attr('data-render-ajax-mustache-template'));
 		var cacheType = $(this).attr('data-render-ajax-cache-type');
+		var ajaxCachingCheckJsId = $(this).attr('data-render-ajax-content-id');
 		
 		
-		var getAndRenderAjaxData = function(){
+		var getAndRenderAjaxData = function(shouldPushToCache){
 			$.get('http://localhost:1338/getevents')
 				.then(function (data) {
 					$renderContentSelector.html(Mustache.render(mustacheTemplate, data[0]));
+					
+					if(shouldPushToCache) {
+						window.app.ajaxCacheService.pushToCache(ajaxCachingCheckJsId, data[0]);
+					}
 				});	
 		};
 		
-		var ajaxCachingCheck = function(){
+		var ajaxCachingCheckJs = function(){
+			var itemFromCache = window.app.ajaxCacheService.pullFromCache(ajaxCachingCheckJsId);
 			
+			if(itemFromCache) {
+				$renderContentSelector.html(Mustache.render(mustacheTemplate, itemFromCache));
+			}
+			else {
+				getAndRenderAjaxData(true);
+			}
 		};
 		
 		switch(cacheType) {
@@ -26,7 +38,7 @@
 				break;
 			case "js":
 				// Caching with javascript construct
-				ajaxCachingCheck();
+				ajaxCachingCheckJs();
 				break;
 			default:
 				// Caching with selector check - if selector is in DOM, then no need to make ajax call again
@@ -42,9 +54,6 @@
 			if ($renderContentSelector.children($(cacheType)).length) { return; }
 			
 		}
-
-		
-		
 
 	});
 
