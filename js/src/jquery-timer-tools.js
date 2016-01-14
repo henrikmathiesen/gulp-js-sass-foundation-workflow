040737eef19e1ea7804e26d6b373a34401875986
Henrik Mathiesen <henrik_mathiesen@hotmail.com>
2016-01-14 10:43:07 +0100

    jquery timer tools test

--- js/src/jquery-timer-tools.js
+++ js/src/jquery-timer-tools.js
@@ -1,5 +1,21 @@
-(function(){
+(function () {
     
-    console.log("jq timer tools");
+    $('html').on('mousemove',
+        $.debounceFirst(3000, function () {
+            $('[data-debounce-first]').append('<div>Mouse move</div>');
+        })
+    );
     
+    $('html').on('mousemove',
+        $.debounceLast(3000, function () {
+            $('[data-debounce-last]').append('<div>Mouse move</div>');
+        })
+    );
+
+    $('html').on('mousemove',
+        $.throttle(3000, function () {
+            $('[data-throttle').append('<div>Mouse move</div>');
+        })
+    );
+
 })();
